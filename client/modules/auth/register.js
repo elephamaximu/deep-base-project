3aca81f537e3ee704b2199d4631025b34ec1d51e
plato <elephamaximu@gmail.com>
2022-04-30 11:02:22 +0900

    리덕스, 사가 초기 뼈대 구성

--- client/modules/auth/register.js
+++ client/modules/auth/register.js
@@ -1,2 +1,19 @@
+import {createAction, handleActions} from 'redux-actions';
+import {call, put, takeLatest} from 'redux-saga/effects';
+import axios from 'axios'
+import {SERVER, headers} from "@/modules/auth/server"
+
+// 상태 초기값
+
+// 액션타입
+
+// 액션생성함수
+
+// 사가 와쳐
+
+// 사가 로직
+
+// 리듀서
+
 const register = {}
 export default register
