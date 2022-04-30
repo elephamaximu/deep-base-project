ece061dec613ec117673ca2cfe0a2a710fe613c0
plato <elephamaximu@gmail.com>
2022-04-30 11:27:23 +0900

    리덕스, 사가 로직 뼈대구성

--- client/modules/auth/login.js
+++ client/modules/auth/login.js
@@ -1,22 +1,51 @@
-import {createAction, handleActions} from 'redux-actions';
-import {call, put, takeLatest} from 'redux-saga/effects';
+import {createAction, handleActions} from 'redux-actions'
+import {call, put, takeLatest} from 'redux-saga/effects'
 import axios from 'axios'
 import {SERVER, headers} from "@/modules/auth/server"
 
 // 상태 초기값
-export const initialstate = {}
+export const initialstate = {
+    loginUser: {},
+    loginError: null,
+    isLoggined: false,
+    token: ''
+}
 
 // 액션타입
 const LOGIN_REQUEST = 'auth/LOGIN_REQUEST'
+const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS'
+const LOGIN_FAILURE = 'auth/LOGIN_FAILURE'
+const LOGIN_CANCELLED = 'auth/LOGIN_CANCELLED'
+const LOGOUT_REQUEST = 'auth/LOGOUT_REQUEST'
+const LOGOUT_SUCCESS = "auth/LOGOUT_SUCCESS"
+const LOGOUT_FAILURE = "auth/LOGOUT_FAILURE"
+const SAVE_TOKEN = 'auth/SAVE_TOKEN'
+const DELETE_TOKEN = 'auth/DELETE_TOKEN'
 
-// 액션생성함수
+// 액션생성함수 -> 외부에서 요청할 수 있는 것만
 export const loginRequest = createAction(LOGIN_REQUEST, data => data)
+export const loginCancelled = createAction(LOGIN_CANCELLED, e => e)
+export const logoutRequest = createAction(LOGOUT_REQUEST, e => e)
 
-// 사가 와쳐
-export function* loginSaga() {}
+// 사가 와쳐 -> 화면만 보는 녀석
+export function* loginSaga() {
+  yield takeLatest(LOGIN_REQUEST, signin)
+  yield takeLatest(LOGIN_CANCELLED, loginCancelled)
+  yield takeLatest(LOGOUT_REQUEST, logout)
+}
 
 // 사가 로직
 function* signin(action) {
+    try {} catch (error) {
+        yield put({})
+    }
+}
+function* loginCancelled() {
+    try {} catch (error) {
+        yield put({})
+    }
+}
+function* logout() {
     try {} catch (error) {
         yield put({})
     }
--- client/modules/auth/register.js
+++ client/modules/auth/register.js
@@ -1,34 +1,54 @@
-import {createAction, handleActions} from 'redux-actions';
-import {call, put, takeLatest} from 'redux-saga/effects';
+import {createAction, handleActions} from 'redux-actions'
+import {call, put, takeLatest} from 'redux-saga/effects'
 import axios from 'axios'
 import {SERVER, headers} from "@/modules/auth/server"
 
 // 상태 초기값
-export const initialstate = {}
+export const initialstate = {
+    isRegistered: false
+}
 
 // 액션타입
 const REGISTER_REQUEST = 'auth/REGISTER_REQUEST'
+const REGISTER_SUCCESS = 'auth/REGISTER_SUCCESS'
+const REGISTER_FAILURE = 'auth/REGISTER_FAILURE'
 
 // 액션생성함수
 export const registerRequest = createAction(REGISTER_REQUEST, data => data)
 
 // 사가 와쳐
-export function* registerSaga() {}
+export function* registerSaga() {
+    yield takeLatest(REGISTER_REQUEST, signup)
+}
 
 // 사가 로직
 function* signup(action) {
-    try {} catch (error) {
-        yield put({})
+    try {
+        console.log(" **** 여기가 핵심 *** " + JSON.stringify(action))
+        const response = yield call(registerAPI, action.payload)
+        console.log(" 회원가입 서버다녀옴: " + JSON.stringify(response.data))
+        yield put({type: REGISTER_SUCCESS, payload: response.data})
+        yield put((window.location.href = "/auth/login"));
+    } catch (error) {
+        yield put({type: REGISTER_FAILURE, payload: error.message})
     }
 }
 
 // API
-const registerAPI = payload => axios.post()
+const registerAPI = payload => axios.post(
+    `${SERVER}/user/join`,
+    payload,
+    {headers}
+)
 
 // 리듀서
 
 const register = (state = initialstate, action) => {
     switch (action.type) {
+        case REGISTER_SUCCESS:
+            return {}
+        case REGISTER_FAILURE:
+            return {}
         default:
             return state
     }
