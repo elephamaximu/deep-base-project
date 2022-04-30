b643cd25e1efae4fe50335d544443856b9b19054
plato <elephamaximu@gmail.com>
2022-04-30 11:11:00 +0900

    리덕스, 사가 초기 뼈대 완성

--- client/modules/auth/login.js
+++ client/modules/auth/login.js
@@ -1,2 +1,36 @@
-const login = {}
+import {createAction, handleActions} from 'redux-actions';
+import {call, put, takeLatest} from 'redux-saga/effects';
+import axios from 'axios'
+import {SERVER, headers} from "@/modules/auth/server"
+
+// 상태 초기값
+export const initialstate = {}
+
+// 액션타입
+const LOGIN_REQUEST = 'auth/LOGIN_REQUEST'
+
+// 액션생성함수
+export const loginRequest = createAction(LOGIN_REQUEST, data => data)
+
+// 사가 와쳐
+export function* loginSaga() {}
+
+// 사가 로직
+function* signin(action) {
+    try {} catch (error) {
+        yield put({})
+    }
+}
+
+// API
+const loginAPI = payload => axios.post()
+
+// 리듀서
+
+const login = (state = initialstate, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
 export default login
--- client/modules/auth/register.js
+++ client/modules/auth/register.js
@@ -4,16 +4,33 @@ import axios from 'axios'
 import {SERVER, headers} from "@/modules/auth/server"
 
 // 상태 초기값
+export const initialstate = {}
 
 // 액션타입
+const REGISTER_REQUEST = 'auth/REGISTER_REQUEST'
 
 // 액션생성함수
+export const registerRequest = createAction(REGISTER_REQUEST, data => data)
 
 // 사가 와쳐
+export function* registerSaga() {}
 
 // 사가 로직
+function* signup(action) {
+    try {} catch (error) {
+        yield put({})
+    }
+}
+
+// API
+const registerAPI = payload => axios.post()
 
 // 리듀서
 
-const register = {}
+const register = (state = initialstate, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
 export default register
